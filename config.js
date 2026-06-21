f0d0a141721f9a4f2497822008cdc24cf53b75a2
siobhankoshodge <siobhankoshodge@gmail.com>
2026-06-21 21:08:35 +0100

    Publish Hodge Holidays planner

---
+++ config.js
@@ -0,0 +1,5 @@
+window.HODGE_HOLIDAYS_CONFIG = {
+  supabaseUrl: "https://rmkxgkoddtbrmubpnoej.supabase.co",
+  supabasePublishableKey: "sb_publishable_DKE7fk255XlCp3q2wizL3Q_Pl_Z4KuW",
+  planId: "hodge-holidays-2026"
+};
